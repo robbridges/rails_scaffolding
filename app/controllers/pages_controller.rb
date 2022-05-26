12683b47c387978ecfc22440114b6b7b46d178e0
Rob Bridges <Rmb101187@gmail.com>
2022-05-26 13:38:17 -0700

    COntroller refactoring. and routes refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,32 @@
 class PagesController < ApplicationController
+    before_action :set_page, except: [:index, :new, :create]
+
     def index
         @pages = Page.all
     end
     def show
-        @page = Page.find(params[:id])
+        
     end
     def new
         @page = Page.new
     end
 
     def create
-        @page = Page.new(page_params)
+        @page =Page.new(page_params)
         @page.save
         redirect_to @page
     end
     def edit
-        @page = Page.find(params[:id])
+        
     end
     def update
-        @page= Page.find(params[:id])
+        
         @page.update(page_params)
         redirect_to @page
     end
 
     def destroy
-        @page=Page.find(params[:id])
+        
         @page.destroy
         redirect_to pages_path
     end
@@ -33,5 +35,9 @@ class PagesController < ApplicationController
 
         def page_params
             params.require(:page).permit(:title, :body, :slug)
+        end
+
+        def set_page
+            @page=Page.find(params[:id])
         end
 end
